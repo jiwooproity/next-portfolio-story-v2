6ba4ad21ed89b0556bdbe4c4d5a33f91af4ed234
jiwooproity <jiwooproity@naver.com>
2023-06-01 23:05:36 +0900

    chore: Next 13 프로젝트 생성 및 레이아웃 / App-Router 설정
    기초 레이아웃 작업과 Next 13 변경점에 대한 확인

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import { Navbar, Footer } from './components'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Web Portfolio v2',
+  description: '웹 포트폴리오 Next 13',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />  
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ app/resume/page.tsx
@@ -0,0 +1,5 @@
+export default function resume() {
+    return (
+        <div className="test">Resume Page!</div>
+    )
+}
