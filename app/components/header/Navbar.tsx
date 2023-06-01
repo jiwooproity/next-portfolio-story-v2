9e9ac18c0802163b8d2d15367ac65af0817120f6
jiwooproity <jiwooproity@naver.com>
2023-06-01 23:05:36 +0900

    chore: Next 13 프로젝트 생성 및 레이아웃 / App-Router 설정
    기초 레이아웃 작업과 Next 13 변경점에 대한 확인

---
+++ app/components/header/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import style from "./navbar.module.css";
+
+interface MenuListIF {
+    to: string,
+    label: string,
+}
+
+const menuList: MenuListIF[] = [
+    { to: './', label: 'Home' },
+    { to: './resume', label: 'Resume' },
+    { to: './contact', label: 'Contact' },
+]
+
+export default function Navbar() {
+    const itemRender = (value: MenuListIF) => {
+        return (
+            <li key={value.label} className={style.menuItem}>
+                <Link href={value.to}>{value.label}</Link>
+            </li>
+        )
+    };
+
+    return (
+        <nav className={style.navContainer}>
+            <div className={style.navWrapper}>
+                <h1 className={style.logo}>Web Portfolio</h1>
+                <ul className={style.menu}>
+                    {menuList.map(itemRender)}
+                </ul>
+            </div>
+        </nav>
+    )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function contact() {
+    return (
+        <div className="test">Contact Page!</div>
+    )
+}
