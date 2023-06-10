67d3eb627ec43f707c5fecd92038f5789232af75
jiwooproity <jiwooproity@naver.com>
2023-06-10 23:44:34 +0900

    style: 포트폴리오 리스트 응답 데이터 가공 및 출력 화면 구현
    가공된 포트폴리오 데이터로 화면의 출력 리스트를 구성. 제대로된 퍼블리싱은 업데이트 예정

--- app/home/rightSide/PortfolioList.tsx
+++ app/home/rightSide/PortfolioList.tsx
@@ -1,15 +1,27 @@
 // 홈 화면 포트폴리오 리스트 출력 준비
-export default function PorfolioList({ data }: { data: any }) {
-  console.log(data);
+import style from "../page.module.css";
 
+interface PortfolioDetailIF {
+  id: string;
+  title: string;
+  description: string;
+  start: string;
+  end: string;
+}
+
+interface PortfolioPropsType {
+  data: PortfolioDetailIF[];
+}
+
+export default function PorfolioList({ data }: PortfolioPropsType) {
   return (
-    <div
-      id="portfolio"
-      className="right-area"
-      style={{ display: "flex", flexDirection: "column" }}
-    >
-      {data.map((item) => (
-        <span style={{ color: "white" }}>{item.title}</span>
+    <div id="portfolio" className={`right-area ${style.portfolioArea}`}>
+      <h2 className={style.title}>Portfolio</h2>
+      {data.map((item: PortfolioDetailIF) => (
+        <>
+          <p className={style.notionTitle}>{item.title}</p>
+          <p className={style.notionDescription}>{item.description}</p>
+        </>
       ))}
     </div>
   );
