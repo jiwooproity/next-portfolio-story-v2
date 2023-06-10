83d35cc2d3f07373b41508d17ca46af70ffab1e3
jiwooproity <jiwooproity@naver.com>
2023-06-10 22:12:50 +0900

    style: 메인 화면 포트폴리오 리스트 출력 구현 준비
    노션 API를 활용한 메인 페이지 포트폴리오 데이터 가공 작업 시작

--- app/home/rightSide/PortfolioList.tsx
+++ app/home/rightSide/PortfolioList.tsx
@@ -1,7 +1,16 @@
-export default function PorfolioList() {
+// 홈 화면 포트폴리오 리스트 출력 준비
+export default function PorfolioList({ data }: { data: any }) {
+  console.log(data);
+
   return (
-    <div id="portfolio" className="right-area">
-      Portfolio Area
+    <div
+      id="portfolio"
+      className="right-area"
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      {data.map((item) => (
+        <span style={{ color: "white" }}>{item.title}</span>
+      ))}
     </div>
   );
 }
