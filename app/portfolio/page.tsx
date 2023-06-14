1c65f53961181a182abac8cf4e86044be1960782
jiwooproity <jiwooproity@naver.com>
2023-06-14 22:19:53 +0900

    modify: 데이터 패칭 Method 스위칭 추가
    GET, POST method에 대한 request 값 구분

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,19 +1,21 @@
 import { getNotionApi } from "../apis/portfolioApi";
 import style from "./page.module.css";
 
-async function getPortfolios() {
-  const res = await getNotionApi();
-  return res.results;
-}
+// async function getPortfolios() {
+//   const res = await getNotionApi();
+//   return res.results;
+// }
 
 export default async function portfolio() {
-  const porfolios = await getPortfolios();
+  // const porfolios = await getPortfolios();
 
-  return (
-    <div className={style.porfolioWrapper}>
-      {porfolios.map((porfolio) => (
-        <div key={porfolio.id}>{porfolio.url}</div>
-      ))}
-    </div>
-  );
+  // return (
+  //   <div className={style.porfolioWrapper}>
+  //     {porfolios.map((porfolio) => (
+  //       <div key={porfolio.id}>{porfolio.url}</div>
+  //     ))}
+  //   </div>
+  // );
+
+  return <div></div>;
 }
