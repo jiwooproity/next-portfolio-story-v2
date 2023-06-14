9c2a46623caf4eed6dddf6f8f047db6ba3279d5b
jiwooproity <jiwooproity@naver.com>
2023-06-14 22:19:53 +0900

    modify: 데이터 패칭 Method 스위칭 추가
    GET, POST method에 대한 request 값 구분

--- app/apis/load.ts
+++ app/apis/load.ts
@@ -16,14 +16,19 @@ export async function load({
   body,
 }: FetchPropsType) {
   try {
-    const response = await fetch(url, {
-      headers,
-      method,
-      body: JSON.stringify(body),
-      next,
-      cache,
-    });
-    return await response.json();
+    let response = null;
+
+    switch (method) {
+      case "GET":
+        response = await fetch(url, { headers, method, next, cache });
+        break;
+      case "POST":
+        response = await fetch(url, { headers, method, body, next, cache });
+      default:
+        break;
+    }
+
+    return await response?.json();
   } catch (e) {
     console.log(e);
   }
