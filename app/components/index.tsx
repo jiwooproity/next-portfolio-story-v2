ddf99020aeb140ae419889662f385155c1b00124
jiwooproity <jiwooproity@naver.com>
2023-07-15 18:29:23 +0900

    publish: 개발자 프로필 웹 사이트 퍼블리싱 작성 중간 업로드

--- app/components/index.tsx
+++
@@ -1,4 +0,0 @@
-import Navbar from "./header/Navbar";
-import Footer from "./footer/Footer";
-
-export { Navbar, Footer };
--- app/resume/page.tsx
+++
@@ -1,5 +0,0 @@
-import style from "./resume.module.css";
-
-export default function resume() {
-  return <div className={style.resumeWrapper}>Resume Page!</div>;
-}
